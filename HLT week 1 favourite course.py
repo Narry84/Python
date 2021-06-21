8339534c956c7568b33b9db483d225959cf52a0e
Narry84 <81918564+Narry84@users.noreply.github.com>
2021-06-21 15:17:06 +0100

    Add files via upload

---
+++ HLT week 1 favourite course.py
@@ -0,0 +1,7 @@
+print("hello world")
+food_1=input("what is your favourite starter?")
+food_2=input("what is your favourite main?")
+food_3=input("what is your favourite dessert")
+food_4=input("what is your favourite drink?")
+new_food=food_1+food_2+food_3+food_4
+print("your favourite meal is....with a glass of....")
---
+++ my first python file.py
@@ -0,0 +1,3 @@
+print("Hisssssss")
+
+print("roar!")
